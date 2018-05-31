b66593b07ed816c259f758bbbcf942918b4f834f
David Fisher <fisherds@rose-hulman.edu>
2018-05-31 11:38:27 +0800

    minor updates

--- NameList/app/src/main/java/edu/rosehulman/fisherds/namelist/MainActivity.java
+++ NameList/app/src/main/java/edu/rosehulman/fisherds/namelist/MainActivity.java
@@ -2,12 +2,10 @@ package edu.rosehulman.fisherds.namelist;
 
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,38 +13,22 @@ public class MainActivity extends AppCompatActivity {
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
-    Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-    setSupportActionBar(toolbar);
+
+    final NameAdapter nameAdapter = new NameAdapter(this);
+    nameAdapter.addName();
+    nameAdapter.addName();
+    nameAdapter.addName();
+    RecyclerView recyclerView = findViewById(R.id.recycler_view);
+    recyclerView.setLayoutManager(new LinearLayoutManager(this));
+    recyclerView.setHasFixedSize(true);
+    recyclerView.setAdapter(nameAdapter);
 
     FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
     fab.setOnClickListener(new View.OnClickListener() {
       @Override
       public void onClick(View view) {
-        Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-            .setAction("Action", null).show();
+        nameAdapter.addName();
       }
     });
-  }
-
-  @Override
-  public boolean onCreateOptionsMenu(Menu menu) {
-    // Inflate the menu; this adds items to the action bar if it is present.
-    getMenuInflater().inflate(R.menu.menu_main, menu);
-    return true;
-  }
-
-  @Override
-  public boolean onOptionsItemSelected(MenuItem item) {
-    // Handle action bar item clicks here. The action bar will
-    // automatically handle clicks on the Home/Up button, so long
-    // as you specify a parent activity in AndroidManifest.xml.
-    int id = item.getItemId();
-
-    //noinspection SimplifiableIfStatement
-    if (id == R.id.action_settings) {
-      return true;
-    }
-
-    return super.onOptionsItemSelected(item);
   }
 }
--- NameList/app/src/main/java/edu/rosehulman/fisherds/namelist/NameAdapter.java
+++ NameList/app/src/main/java/edu/rosehulman/fisherds/namelist/NameAdapter.java
@@ -3,14 +3,16 @@ package edu.rosehulman.fisherds.namelist;
 import android.content.Context;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
-import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
-public class NameAdapter extends RecyclerView.Adapter {
+public class NameAdapter extends RecyclerView.Adapter<NameAdapter.NameViewHolder> {
   private List<String> mNames;
   private Context mContext;
   private Random mRandom = new Random();
@@ -18,11 +20,6 @@ public class NameAdapter extends RecyclerView.Adapter {
   public NameAdapter(Context context) {
     mNames = new ArrayList<>();
     mContext = context;
-
-    for (int i = 0; i < 5; i++) {
-      mNames.add(getRandomName());
-    }
-    Log.d("TAG", "Names = " + mNames);
   }
 
   private String getRandomName() {
@@ -35,19 +32,43 @@ public class NameAdapter extends RecyclerView.Adapter {
     return names[mRandom.nextInt(names.length)];
   }
 
+  public void addName() {
+//    mNames.add(getRandomName());
+//    notifyDataSetChanged();
+
+    mNames.add(0, getRandomName());
+//    notifyItemInserted(0);
+    notifyDataSetChanged();
+  }
+
   @NonNull
   @Override
-  public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-    return null;
+  public NameViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+    View itemView = LayoutInflater.from(parent.getContext()).inflate(R.layout.name_view, parent, false);
+    return new NameViewHolder(itemView);
   }
 
   @Override
-  public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
-
+  public void onBindViewHolder(@NonNull NameViewHolder holder, int position) {
+    String name = mNames.get(position);
+    holder.mNameTextView.setText(name);
+    holder.mDescriptionTextView.setText(String.format("I'm #%d", (position+1)));
   }
 
   @Override
   public int getItemCount() {
-    return 0;
+    return mNames.size();
   }
+
+  class NameViewHolder extends RecyclerView.ViewHolder {
+    private final TextView mNameTextView;
+    private final TextView mDescriptionTextView;
+
+    public NameViewHolder(View itemView) {
+      super(itemView);
+      mNameTextView = itemView.findViewById(R.id.name);
+      mDescriptionTextView = itemView.findViewById(R.id.description);
+    }
+  }
+
 }
